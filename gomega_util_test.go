ecff92a79d39e0bdb018ce1e91cb985a8462be75
David Capwell <dcapwell@gopivotal.com>
2013-11-19 09:52:44 -0800

    no longer using len, but converting to int

--- gomega_util_test.go
+++ gomega_util_test.go
@@ -14,12 +14,6 @@ func lengthOf(a interface{}) (int, bool) {
 	switch reflect.TypeOf(a).Kind() {
 	case reflect.Map, reflect.Array, reflect.String, reflect.Chan, reflect.Slice:
 		return reflect.ValueOf(a).Len(), true
-	case reflect.Int:
-		data, ok := a.(int)
-		if ok {
-			return data, true
-		}
-		return 0, false
 	default:
 		return 0, false
 	}
@@ -53,25 +47,53 @@ func isString(a interface{}) bool {
 	return reflect.TypeOf(a).Kind() == reflect.String
 }
 
-func BeGreaterThan(num int) OmegaMatcher {
+func isInteger(a interface{}) bool {
+	kind := reflect.TypeOf(a).Kind()
+	return reflect.Int <= kind && kind <= reflect.Int64
+}
+
+func toInteger(a interface{}) int64 {
+	if isInteger(a) {
+		return reflect.ValueOf(a).Int()
+	} else if isUnsignedInteger(a) {
+		return int64(reflect.ValueOf(a).Uint())
+	} else if isFloat(a) {
+		return int64(reflect.ValueOf(a).Float())
+	} else {
+		panic(fmt.Sprintf("Expected a number!  Got <%T> %#v", a, a))
+	}
+}
+
+func isUnsignedInteger(a interface{}) bool {
+	kind := reflect.TypeOf(a).Kind()
+	return reflect.Uint <= kind && kind <= reflect.Uint64
+}
+
+func isFloat(a interface{}) bool {
+	kind := reflect.TypeOf(a).Kind()
+	return reflect.Float32 <= kind && kind <= reflect.Float64
+}
+
+func BeGreaterThan(num int64) OmegaMatcher {
 	return &GreaterThanMatcher{
 		Num: num,
 	}
 }
 
 type GreaterThanMatcher struct {
-	Num int
+	Num int64
 }
 
 func (matcher *GreaterThanMatcher) Match(actual interface{}) (success bool, message string, err error) {
-	length, ok := lengthOf(actual)
+	ok := isInteger(actual)
 	if ok {
-		if length > matcher.Num {
-			return true, fmt.Sprintf("Expected%s\n (length: %d) not to be greater than length %d", formatObject(actual), length, matcher.Num), nil
+		num := toInteger(actual)
+		if num > matcher.Num {
+			return true, fmt.Sprintf("Expected\n (%d) not to be greater than %d", num, matcher.Num), nil
 		} else {
-			return false, fmt.Sprintf("Expected%s\n (length: %d) to be greater than length %d", formatObject(actual), length, matcher.Num), nil
+			return false, fmt.Sprintf("Expected\n (%d) to be greater than %d", num, matcher.Num), nil
 		}
 	} else {
-		return false, "", fmt.Errorf("GreaterThan matcher expects a int/string/array/map/channel/slice.  Got:%s", formatObject(actual))
+		return false, "", fmt.Errorf("GreaterThan matcher expects a int based type.  Got:%s", formatObject(actual))
 	}
 }
