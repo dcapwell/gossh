8c7627d966d5500595b8e8b19c70fc6df75dd426
David Capwell <dcapwell@gopivotal.com>
2013-11-22 18:40:44 -0800

    got hacky http server running

--- gossh_agent/main.go
+++ gossh_agent/main.go
@@ -1,16 +1,64 @@
 package main
 
 import (
+  "code.google.com/p/gorest"
+  "github.com/dcapwell/gossh"
+  "github.com/dcapwell/gossh/utils"
   "log"
   "net/http"
   "fmt"
-  "html"
 )
 
-func main() {
-  http.HandleFunc("/ssh", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-  })
+type SshService struct {
+  gorest.RestService `root:"/ssh/"`
 
+  hosts   gorest.EndPoint `method:"GET" path:"/{hosts:string}/" output:"SshCmdResponses"`
+}
+
+type SshCmdResponses struct {
+  Responses []gossh.SshResponseContext
+}
+
+func WaitFor(ch chan gossh.SshResponseContext) []gossh.SshResponseContext {
+  data := make([]gossh.SshResponseContext, 0)
+
+  for d := range ch {
+    data = append(data, d)
+  }
+
+  return data
+}
+
+func (serv SshService) Hosts(hosts string) SshCmdResponses {
+  ssh := gossh.NewSsh()
+  h, err := utils.Expand(hosts)
+  if err != nil {
+    badResponse(serv, err)
+    return SshCmdResponses{}
+  }
+  rsp, err := ssh.Run(h, "date", gossh.Options{})
+  if err != nil {
+    badResponse(serv, err)
+    return SshCmdResponses{}
+  }
+  /*
+  for ctx := range rsp.Responses {
+    log.Printf("Hostname: %s\nStdout: %s\nStderr: %s\n", ctx.Hostname, ctx.Response.Stdout, ctx.Response.Stderr)
+  }
+  */
+  //return rsp
+  return SshCmdResponses{WaitFor(rsp.Responses)}
+}
+
+func badResponse(serv SshService, err error) {
+  serv.ResponseBuilder().SetResponseCode(400).WriteAndOveride([]byte(fmt.Sprintf("%v", err)))
+}
+
+
+func main() {
+  // register with gorest
+  gorest.RegisterService(new(SshService))
+
+  http.Handle("/",gorest.Handle())
   log.Fatal(http.ListenAndServe(":7654", nil))
 }
---
+++ prototype/http-server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+  "code.google.com/p/gorest"
+  "log"
+  "net/http"
+)
+
+type SshService struct {
+  gorest.RestService `root:"/ssh/"`
+
+  helloWorld  gorest.EndPoint `method:"GET" path:"/hello-world/" produces:"shouldfreakout" output:"string"`
+  helloWorldTwo  gorest.EndPoint `method:"GET" path:"/hello-world-2/" output:"string"`
+  serde       gorest.EndPoint `method:"GET" path:"/serde/" output:"map[string]string"`
+}
+
+func (serv SshService) HelloWorld() string {
+  return "Hello World!"
+}
+
+func (serv SshService) HelloWorldTwo() string {
+  serv.ResponseBuilder().
+    AddHeader("Access-Control-Allow-Origin","http://127.0.0.1:8888").
+    AddHeader("Access-Control-Allow-Headers","X-HTTP-Method-Override").
+    AddHeader("Access-Control-Allow-Headers","X-Xsrf-Cookie").
+    AddHeader("Access-Control-Expose-Headers","X-Xsrf-Cookie").
+    SetResponseCode(200).
+    SetContentType(gorest.Text_RichText)
+  return "Hello World! Two!"
+}
+
+func (serv SshService) Serde() map[string]string {
+  data := make(map[string]string)
+  data["Hello"] = "World!"
+  return data
+}
+
+func (serv SshService) Builder() {
+  rb := serv.RB()
+  rb.Write([]byte("Hello World"))
+}
+
+func main() {
+  // register with gorest
+  gorest.RegisterService(new(SshService))
+
+  http.Handle("/",gorest.Handle())
+  log.Fatal(http.ListenAndServe(":7654", nil))
+}
