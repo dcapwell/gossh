d2ec72a9cf49457af78957e84ce3a523913bef3e
David Capwell <dcapwell@gopivotal.com>
2013-11-25 09:53:02 -0800

    added flags to http agent

--- gossh_agent/main.go
+++ gossh_agent/main.go
@@ -7,6 +7,9 @@ import (
   "log"
   "net/http"
   "fmt"
+  "flag"
+  "os"
+  "io/ioutil"
   "strconv"
   "strings"
 )
@@ -21,7 +24,6 @@ type SshCmdResponses struct {
   Responses []gossh.SshResponseContext
 }
 
-
 func (serv SshService) Hosts(hosts string) SshCmdResponses {
   // check for allowed query params
   cmd, opts, err := serv.parseQuery()
@@ -31,11 +33,11 @@ func (serv SshService) Hosts(hosts string) SshCmdResponses {
   ssh := gossh.NewSsh()
   h, err := utils.Expand(hosts)
   if err != nil {
-    serv.badResponse(err)
+    return serv.badResponse(err)
   }
   rsp, err := ssh.Run(h, cmd, opts)
   if err != nil {
-    serv.badResponse(err)
+    return serv.badResponse(err)
   }
   return SshCmdResponses{waitFor(rsp.Responses)}
 }
@@ -95,9 +97,20 @@ func (serv SshService) badResponse(err error) SshCmdResponses {
 }
 
 func main() {
+  flags := flag.NewFlagSet("Gossh Agent", flag.ContinueOnError)
+  flags.SetOutput(ioutil.Discard)
+  base := "/"
+  // looks like gorest doesn't work well unless at /
+  // flags.StringVar(&base, "base", "/", "base path for http requests; defaults to '/'")
+  port := ":7654"
+  flags.StringVar(&port, "port", ":7654", "port to listen on; defaults to ':7654'")
+  if err := flags.Parse(os.Args[1:]); err != nil {
+    log.Fatal(err)
+  }
+
   // register with gorest
   gorest.RegisterService(new(SshService))
 
-  http.Handle("/",gorest.Handle())
-  log.Fatal(http.ListenAndServe(":7654", nil))
+  http.Handle(base,gorest.Handle())
+  log.Fatal(http.ListenAndServe(port, nil))
 }
