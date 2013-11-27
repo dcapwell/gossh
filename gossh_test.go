81b471aacf9bbca7271927200c664419443c291f
David Capwell <dcapwell@gopivotal.com>
2013-11-27 09:52:25 -0800

    nest ssh tests so its easier to add tests that talk to different interfaces

--- gossh_test.go
+++ gossh_test.go
@@ -9,99 +9,101 @@ import (
 )
 
 var _ = Describe("Gossh", func() {
-	ssh := NewSsh()
+  Describe("multi hosts", func() {
+    ssh := NewSsh()
 
-	Describe("run multiple ssh commands concurrently", func() {
-		Context("with one host", func() {
-			Context("and valid command", func() {
-				rsp, err := ssh.Run([]string{"localhost"}, "date", Options{})
-				data := chanToSlize(rsp.Responses)
+    Describe("run multiple ssh commands concurrently", func() {
+      Context("with one host", func() {
+        Context("and valid command", func() {
+          rsp, err := ssh.Run([]string{"localhost"}, "date", Options{})
+          data := chanToSlize(rsp.Responses)
 
-				It("should succeed", func() {
-					Expect(err).To(BeNil())
-				})
+          It("should succeed", func() {
+            Expect(err).To(BeNil())
+          })
 
-				It("with one result", func() {
-					Expect(data).To(HaveLen(1))
-					Expect(data[0].Hostname).To(Equal("localhost"))
-					Expect(data[0].Response.Code).To(Equal(workpool.SUCCESS))
-					Expect(data[0].Response.Stdout).To(ContainSubstring("PST"))
-				})
-			})
-			Context("with invalid command", func() {
-				rsp, err := ssh.Run([]string{"localhost"}, "thiscmdreallyshouldntexist", Options{})
-				data := chanToSlize(rsp.Responses)
+          It("with one result", func() {
+            Expect(data).To(HaveLen(1))
+            Expect(data[0].Hostname).To(Equal("localhost"))
+            Expect(data[0].Response.Code).To(Equal(workpool.SUCCESS))
+            Expect(data[0].Response.Stdout).To(ContainSubstring("PST"))
+          })
+        })
+        Context("with invalid command", func() {
+          rsp, err := ssh.Run([]string{"localhost"}, "thiscmdreallyshouldntexist", Options{})
+          data := chanToSlize(rsp.Responses)
 
-				It("should not return error", func() {
-					Expect(err).To(BeNil())
-				})
-				It("should have only one response", func() {
-					Expect(data).To(HaveLen(1))
-				})
-				It("should have response from host", func() {
-					localRsp := data[0]
-					Expect(localRsp.Hostname).To(Equal("localhost"))
-				})
-				It("should have failed", func() {
-					localRsp := data[0]
-					Expect(localRsp.Response).ShouldNot(BeNil())
-					Expect(localRsp.Response.Code).To(BeGreaterThan(0))
-				})
-			})
-		})
-		Context("with mulitple hosts", func() {
-			Context("with valid command", func() {
-				rsp, err := ssh.Run([]string{"localhost", "localhost"}, "date", Options{})
-				data := chanToSlize(rsp.Responses)
+          It("should not return error", func() {
+            Expect(err).To(BeNil())
+          })
+          It("should have only one response", func() {
+            Expect(data).To(HaveLen(1))
+          })
+          It("should have response from host", func() {
+            localRsp := data[0]
+            Expect(localRsp.Hostname).To(Equal("localhost"))
+          })
+          It("should have failed", func() {
+            localRsp := data[0]
+            Expect(localRsp.Response).ShouldNot(BeNil())
+            Expect(localRsp.Response.Code).To(BeGreaterThan(0))
+          })
+        })
+      })
+      Context("with mulitple hosts", func() {
+        Context("with valid command", func() {
+          rsp, err := ssh.Run([]string{"localhost", "localhost"}, "date", Options{})
+          data := chanToSlize(rsp.Responses)
 
-				It("should not return error", func() {
-					Expect(err).To(BeNil())
-				})
-				It("should have only two response", func() {
-					Expect(data).To(HaveLen(2))
-				})
-				It("should have response from host", func() {
-					localRsp := data[0]
-					Expect(localRsp.Hostname).To(Equal("localhost"))
-					localRsp = data[1]
-					Expect(localRsp.Hostname).To(Equal("localhost"))
-				})
-				It("should have successed", func() {
-					Expect(data[0].Response.Code).To(Equal(workpool.SUCCESS))
-					Expect(data[0].Response.Stdout).To(ContainSubstring("PST"))
+          It("should not return error", func() {
+            Expect(err).To(BeNil())
+          })
+          It("should have only two response", func() {
+            Expect(data).To(HaveLen(2))
+          })
+          It("should have response from host", func() {
+            localRsp := data[0]
+            Expect(localRsp.Hostname).To(Equal("localhost"))
+            localRsp = data[1]
+            Expect(localRsp.Hostname).To(Equal("localhost"))
+          })
+          It("should have successed", func() {
+            Expect(data[0].Response.Code).To(Equal(workpool.SUCCESS))
+            Expect(data[0].Response.Stdout).To(ContainSubstring("PST"))
 
-					Expect(data[1].Response.Code).To(Equal(workpool.SUCCESS))
-					Expect(data[1].Response.Stdout).To(ContainSubstring("PST"))
-				})
-			})
-			Context("with invalid command", func() {
-				rsp, err := ssh.Run([]string{"localhost", "localhost"}, "thiscmdreallyshouldntexist", Options{})
-				data := chanToSlize(rsp.Responses)
+            Expect(data[1].Response.Code).To(Equal(workpool.SUCCESS))
+            Expect(data[1].Response.Stdout).To(ContainSubstring("PST"))
+          })
+        })
+        Context("with invalid command", func() {
+          rsp, err := ssh.Run([]string{"localhost", "localhost"}, "thiscmdreallyshouldntexist", Options{})
+          data := chanToSlize(rsp.Responses)
 
-				log.Printf("Response from multi requests, single host, invalid cmd: %v\n", rsp)
+          log.Printf("Response from multi requests, single host, invalid cmd: %v\n", rsp)
 
-				It("should not return error", func() {
-					Expect(err).To(BeNil())
-				})
-				It("should have only two response", func() {
-					Expect(data).To(HaveLen(2))
-				})
-				It("should have response from host", func() {
-					Expect(data[0].Hostname).To(Equal("localhost"))
-					Expect(data[1].Hostname).To(Equal("localhost"))
-				})
-				It("should have failed", func() {
-					localRsp := data[0]
-					Expect(localRsp.Response).ShouldNot(BeNil())
-					Expect(localRsp.Response.Code).To(BeGreaterThan(0))
+          It("should not return error", func() {
+            Expect(err).To(BeNil())
+          })
+          It("should have only two response", func() {
+            Expect(data).To(HaveLen(2))
+          })
+          It("should have response from host", func() {
+            Expect(data[0].Hostname).To(Equal("localhost"))
+            Expect(data[1].Hostname).To(Equal("localhost"))
+          })
+          It("should have failed", func() {
+            localRsp := data[0]
+            Expect(localRsp.Response).ShouldNot(BeNil())
+            Expect(localRsp.Response.Code).To(BeGreaterThan(0))
 
-					localRsp = data[1]
-					Expect(localRsp.Response).ShouldNot(BeNil())
-					Expect(localRsp.Response.Code).To(BeGreaterThan(0))
-				})
-			})
-		})
-	})
+            localRsp = data[1]
+            Expect(localRsp.Response).ShouldNot(BeNil())
+            Expect(localRsp.Response.Code).To(BeGreaterThan(0))
+          })
+        })
+      })
+    })
+  })
 })
 
 func chanToSlize(ch chan SshResponseContext) []SshResponseContext {
